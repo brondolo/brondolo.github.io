8f628e86701ef6b88bb95fdd93862bdd57f5d989
Brian <brianrondolo@yahoo.com>
2023-11-17 15:44:54 -0800

    add icon to manifest and service worker

--- CNIT133M/Homework_4/service-worker.js
+++ CNIT133M/Homework_4/service-worker.js
@@ -5,7 +5,7 @@ self.addEventListener('install', (event) => {
                 '/',
                 '/index.html',
                 '/manifest.json',
-                '/images/lightblue.jpg'
+                '/images/dog-icon-32.png'
             ]);
         })
     );
